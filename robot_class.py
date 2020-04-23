b12b321e105f1ea4fefdae5bc7536190a9786547
nik1806 <nikhil.paliwal14@gmail.com>
2020-04-23 17:19:54 +0530

    add: sense function in robot_class

--- robot_class.py
+++ robot_class.py
@@ -86,11 +86,27 @@ class robot:
         ## 2. account for measurement noise by *adding* a noise component to dx and dy
         ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
         ##    - Feel free to use the function self.rand() to help calculate this noise component
+        ##    - It may help to reference the `move` function for noise calculation
         ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
         ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
         
         ## TODO: return the final, complete list of measurements
+        for i in range(self.num_landmarks):
+            # calculating the absolute difference
+            dx = self.landmarks[i][0] - self.x
+            dy = self.landmarks[i][1] - self.y
+            
+            # accounting the measurement erros
+            dx += self.rand() * self.measurement_noise
+            dy += self.rand() * self.measurement_noise
+            
+            # checking the validity of measurements
+            if abs(dx) > self.measurement_range or abs(dy) > self.measurement_range:
+                continue # ignoring the current readings as landmark out of reach
+                
+            measurements.append([i, dx, dy])
+            
         return measurements
 
 
